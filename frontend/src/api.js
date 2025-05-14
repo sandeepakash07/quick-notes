3dd9a0e5440848cb400a5819f3abca6be96ed001
Sandeep Akash <sandeepakash1601@gmail.com>
2025-05-14 18:41:56 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API=axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL: import.meta.env.VITE_API_BASE_URL||"http://localhost:5000"
 })
 
 export default API;
